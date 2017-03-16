43cbb2def2a7424288d7de9c78ca877fd23362a7
JStaler <josef.staler@gmail.com>
2017-03-16 11:54:07 -0300

    Update iControl_MainScript.js

--- iControl_MainScript.js
+++ iControl_MainScript.js
@@ -583,7 +583,7 @@ if (avisoOS != null) {
     
     //document.getElementById("loadingText").innerHTML = '<P>Bonus Indo Embora...</P>';
 
-    replacejscssfile("../Styles/load.css", "https://cdn.rawgit.com/JStaler/iControl/dcd8bffc/load.css", "css");
+    replacejscssfile("../Styles/load.css", "https://cdn.rawgit.com/JStaler/iControl/6272e7c3/load.css", "css");
     //document.getElementById("loadingText").innerHTML = '<p>Processando...</p>';
 
     //removejscssfile("../Scripts/PageLoader.js", "js")
